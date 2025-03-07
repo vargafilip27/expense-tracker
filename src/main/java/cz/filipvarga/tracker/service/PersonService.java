d4bf3908966fc2be2a28779fbc4d394dfd24ac06
Filip Varga <vargafi2@fit.cvut.cz>
2025-03-07 21:15:45 +0100

    Implement PersonService

--- src/main/java/cz/filipvarga/tracker/service/PersonService.java
+++ src/main/java/cz/filipvarga/tracker/service/PersonService.java
@@ -1,9 +1,55 @@
 package cz.filipvarga.tracker.service;
 
+import cz.filipvarga.tracker.domain.Person;
+import cz.filipvarga.tracker.persistent.JPAPersonRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PersonService implements PersonServiceInterface {
 
+    private final JPAPersonRepository personRepository;
 
+    public PersonService(JPAPersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @Override
+    public Person createPerson(Person person) throws IllegalArgumentException {
+        if (personRepository.existsById(person.getId())) {
+            throw new IllegalArgumentException("Person with id " + person.getId() + " already exists");
+        }
+        else return personRepository.save(person);
+    }
+
+    @Override
+    public Person getPersonById(Long id) throws EntityNotFoundException {
+        if (personRepository.existsById(id)) {
+            return personRepository.findById(id).get();
+        }
+        else throw new EntityNotFoundException("Person with id " + id + " not found");
+    }
+
+    @Override
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+
+    @Override
+    public Person updatePerson(Person person) throws IllegalArgumentException {
+        if (personRepository.existsById(person.getId())) {
+            return personRepository.save(person);
+        }
+        else throw new IllegalArgumentException("Person with id " + person.getId() + " not found");
+    }
+
+    @Override
+    public void deletePerson(Long id) throws EntityNotFoundException {
+        if (personRepository.existsById(id)) {
+            personRepository.deleteById(id);
+        }
+        else throw new EntityNotFoundException("Person with id " + id + " not found");
+    }
 }
--- src/main/java/cz/filipvarga/tracker/service/PersonServiceInterface.java
+++ src/main/java/cz/filipvarga/tracker/service/PersonServiceInterface.java
@@ -1,4 +1,16 @@
 package cz.filipvarga.tracker.service;
 
+import cz.filipvarga.tracker.domain.Expense;
+import cz.filipvarga.tracker.domain.Person;
+import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
+
 public interface PersonServiceInterface {
+
+    Person createPerson(Person person) throws IllegalArgumentException; // CREATE
+    Person getPersonById(Long id) throws EntityNotFoundException;       // READ
+    List<Person> getAllPersons();
+    Person updatePerson(Person person) throws IllegalArgumentException; // UPDATE
+    void deletePerson(Long id) throws EntityNotFoundException;          // DELETE
 }
