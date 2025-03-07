386cbe394347856b2e322cb19e39d3efd67b43ca
Filip Varga <vargafi2@fit.cvut.cz>
2025-03-07 12:57:17 +0100

    Refactor app

--- src/main/java/cz/filipvarga/tracker/TrackerApplication.java
+++ src/main/java/cz/filipvarga/tracker/TrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExpenseApplication {
+public class TrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExpenseApplication.class, args);
+		SpringApplication.run(TrackerApplication.class, args);
 	}
 
 }
